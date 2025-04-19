bb01c097999098bdba6a7f2ca721bf182f301313
shiohira-masaya <shiohira-masaya@dmm.com>
2025-04-20 01:13:31 +0900

    refill処理のテストを追加

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,27 +4,31 @@ from domain.models.money import Money
 from domain.models.slot import Slot
 from domain.models.vending_machine import VendingMachine
 
-slots = {
-    "A1": Slot(
-        slot_number="A1",
-        drink=Drink(name="Coke", price=120, temperature=Temperature.COLD),
-        stock=5
-    ),
-    "B1": Slot(
-        slot_number="B1",
-        drink=Drink(name="Pepsi", price=130, temperature=Temperature.COLD),
-        stock=0
-    ),
-}
+
+@pytest.fixture(scope="function")
+def fresh_slots():
+    slots = {
+        "A1": Slot(
+            slot_number="A1",
+            drink=Drink(name="Coke", price=120, temperature=Temperature.COLD),
+            stock=5
+        ),
+        "B1": Slot(
+            slot_number="B1",
+            drink=Drink(name="Pepsi", price=130, temperature=Temperature.COLD),
+            stock=0
+        ),
+    }
+    return slots
 
 
-@pytest.fixture
-def vending_machine_with_no_money():
-    return VendingMachine(slots=slots, cash_reserve=Money(1000))
+@pytest.fixture(scope="function")
+def vending_machine_with_no_money(fresh_slots):
+    return VendingMachine(slots=fresh_slots, cash_reserve=Money(1000))
 
 
-@pytest.fixture
-def vending_machine_with_inserted_money():
-    vending_machine = VendingMachine(slots=slots, cash_reserve=Money(1000))
+@pytest.fixture(scope="function")
+def vending_machine_with_inserted_money(fresh_slots):
+    vending_machine = VendingMachine(slots=fresh_slots, cash_reserve=Money(1000))
     vending_machine.insert_money(Money(200))
     return vending_machine
